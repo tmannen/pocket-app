ef92e7c5b1a7f904a9d49510578f56184d485a41
unknown <tanelille@hotmail.com>
2017-02-04 19:22:12 +0200

    search cool

--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -3,25 +3,25 @@ import '../src/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import $ from 'jquery';
 import _ from 'lodash';
+import moment from 'moment';
 import SearchBar from 'SearchBar'
 
 class Search extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {data: {}, searchInput: this.props.location.query};
-        this.queryServer = this.queryServer.bind(this);
+        this.state = {data: {}, query: this.props.location.query.q};
+        this.queryServer.bind(this);
     }
 
     componentDidMount() {
-        this.queryServer(); //how to make this run when using the search bar in /search?
+        this.queryServer(this.props.location.query.q);
     }
 
-    queryServer() {
-        console.log(this.state.searchInput)
-        return $.getJSON('http://127.0.0.1:5000/search?q='+this.state.searchInput.q)
+
+    queryServer(query) {
+        return $.getJSON('http://127.0.0.1:5000/search?q='+query)
             .then((data) => {
-                console.log(data)
                 this.setState({ data: data });
             });
     }
@@ -30,11 +30,29 @@ class Search extends Component {
 
         return (
             <div className="container">
-                <SearchBar/>
-
-                {_.map(this.state.data.results, function(object, i){
-                    return <span className="tag-label" key={i}>{object}</span>
-                })}
+                <SearchBar clickFunction={this.queryServer.bind(this)}/>
+                <div className="col-sm-12 thumbnail-container">
+                    {_.map(this.state.data.results, function(object, i){
+                            return <div className="col-sm-3">
+                                        <div className="thumbnail" key={i}>
+                                            <div className="caption">
+                                                <h3><a href={object.resolved_url}>{object.resolved_title}</a></h3>
+                                                <p>{object.excerpt}</p>
+                                            </div>
+                                            <div className="icon-holder">
+                                                <div className="glyphicon glyphicon-tags search-tag"></div>
+                                                {_.map(object.tags, function(tag, index){
+                                                    return <div className="object-tag">{index}</div>;
+                                                })}
+                                            </div>
+                                            <div className="icon-holder-time">
+                                                <div className="glyphicon glyphicon-time"></div>
+                                                {moment.unix(object.time_added).format('DD.MM.YYYY')}
+                                            </div>
+                                        </div>
+                                   </div>
+                    })}
+                </div>
             </div>
         );
     }
